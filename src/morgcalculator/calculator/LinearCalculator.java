7a27b72281bd5ddd72a85c504ec7578300c70d87
Gypra <gytisnaujasis@icloud.com>
2025-10-15 20:31:40 +0300

    add unfinished linear calculation

--- src/morgcalculator/calculator/LinearCalculator.java
+++ src/morgcalculator/calculator/LinearCalculator.java
@@ -18,17 +18,37 @@ public class LinearCalculator extends MortgageCalculator {
 		float periodRate = getRateForPeriod(rate);
 		int periodCount = getNumberOfPeriods();
 		float loanPayment = getLoanAmount() / periodCount;
-
 		float balance = getLoanAmount();
-
 		LocalDate startDate = LocalDate.now();
 
+		Defer defer = getDefer();
+		LocalDate deferStartDate = (defer != null) ? defer.getDate() : null;
+		LocalDate deferEndDate = (defer != null) ? deferStartDate.plusMonths(defer.getLengthMonths()) : null;
+		float deferRate = (defer != null) ? defer.getRate() / 100f : 1f;
+
+//		if (defer != null) {
+//			periodCount += defer.getLengthMonths();
+//			loanPayment = getLoanAmount() / periodCount;
+//		}
+
 		for (int i = 0; i < periodCount; i++) {
+			boolean isDeferement = defer != null && !startDate.isBefore(deferStartDate)
+					&& !startDate.isAfter(deferEndDate);
+
 			float interest = balance * periodRate;
 			float totalPayment = loanPayment + interest;
-			balance -= loanPayment;
+			float principal = loanPayment;
 
-			Payment payment = new Payment(i + 1, startDate, rate, interest, loanPayment, totalPayment);
+			if (isDeferement) {
+				totalPayment *= deferRate;
+				principal = totalPayment - interest;
+				System.out.println("principal: " + principal);
+				System.out.println("to pay: " + loanPayment);
+				System.out.println("leftover:" + (loanPayment - principal));
+			}
+			balance -= principal;
+
+			Payment payment = new Payment(i + 1, startDate, rate, interest, principal, totalPayment);
 			payments.add(payment);
 
 			startDate = startDate.plusMonths(1);
